90a989cd3a7db352caed88c35e70282e7e6bf9a5
Samn Nguyễn <2npa2812@gmail.com>
2017-07-26 15:32:12 +0700

    update store file
    Separate code Store proc and Exec store proc

---
+++ Source/EXEC SP.sql
@@ -0,0 +1,95 @@
+﻿-- exec Procedure
+
+USE DRCARE
+GO
+
+-- ADD MEDICAL_RECORD
+EXEC dbo.sp_MEDICAL_RECORD_DETAILS_AddNewMedicalRecord @MecRcID = 11, -- int
+    @DiseaseID = 6, -- int
+    @DoctorID = 1 -- int
+
+EXEC dbo.sp_MEDICAL_RECORD_DETAILS_AddNewMedicalRecord @MecRcID = 10, -- int
+    @DiseaseID = 2, -- int
+    @DoctorID = 4 -- int
+
+EXEC dbo.sp_MEDICAL_RECORD_DETAILS_AddNewMedicalRecord @MecRcID = 9, -- int
+    @DiseaseID = 4, -- int
+    @DoctorID = 4 -- int
+GO
+
+
+-- addd precription
+EXEC dbo.sp_PRESCRIPTION_AddNewIncription @MecRcDtID = 1, -- int
+    @MedID = 6, -- int
+    @MedQty = 1.0, -- float
+    @TimeTakeMedicine = 3, -- smallint
+    @SumMedQty = 21 -- smallint
+
+
+EXEC dbo.sp_PRESCRIPTION_AddNewIncription @MecRcDtID = 1, -- int
+    @MedID = 3, -- int
+    @MedQty = 1.0, -- float
+    @TimeTakeMedicine = 2, -- smallint
+    @SumMedQty = 14 -- smallint
+
+
+EXEC dbo.sp_PRESCRIPTION_AddNewIncription @MecRcDtID = 1, -- int
+    @MedID = 12, -- int
+    @MedQty = 1.0, -- float
+    @TimeTakeMedicine = 2, -- smallint
+    @SumMedQty = 14 -- smallint
+
+
+EXEC dbo.sp_PRESCRIPTION_AddNewIncription @MecRcDtID = 2, -- int
+    @MedID = 9, -- int
+    @MedQty = 1.0, -- float
+    @TimeTakeMedicine = 2, -- smallint
+    @SumMedQty = 12 -- smallint
+
+
+EXEC dbo.sp_PRESCRIPTION_AddNewIncription @MecRcDtID = 2, -- int
+    @MedID = 14, -- int
+    @MedQty = 1.0, -- float
+    @TimeTakeMedicine = 1, -- smallint
+    @SumMedQty = 6 -- smallint
+GO
+
+-- add remind
+
+
+-- GET METHOD	
+
+/* GET Medical Method Details */
+-- [DOCTOR] Lấy Danh sách Bệnh nhân (MedicalRecord) 
+-- param IN: @DoctorID + @DayCreated
+-- Result: đã được sort theo thứ tự trước sau (FIFO)
+EXEC dbo.sp_Get_Search_MedicalRecordDetails 
+	@MecRcID = NULL, -- int
+    @DoctorID = 4, -- int
+    @DayCreated = '26/07/2017' -- VARCHAR(15)
+
+
+-- [PATIENT] Lấy ALL phiếu bệnh của Người đó 
+-- param IN: @MecRcID (ID of Patient)
+EXEC dbo.sp_Get_Search_MedicalRecordDetails
+	@MecRcID = 11, -- int
+    @DoctorID = NULL, -- int
+    @DayCreated = NULL -- varchar(15)
+
+-- [PATIENT] Lấy ALL phiếu bệnh của Người đó 
+-- param IN: @MecRcID (ID of Patient) + @DayCreated (ngày muốn search)
+EXEC dbo.sp_Get_Search_MedicalRecordDetails 
+	@MecRcID = 11, -- int
+    @DoctorID = NULL, -- int
+    @DayCreated = '19/06/2017' -- varchar(15)
+
+/** GET Đơn thuốc theo 1 Phiếu khám được nhất định */
+EXEC dbo.sp_getPrecriptionByMecRcDetailsID @MecRcDtID = 2 -- int
+GO
+
+EXEC dbo.sp_getPrecriptionByMecRcDetailsID @MecRcDtID = 1 -- int
+GO
+
+/* GET List Nhắc nhở của 1 Phiếu khám được nhất định*/
+EXEC dbo.sp_getRemindListByMecRcDetailsID @MecRcDetailsID = 1  -- int
+GO
--- Source/Query_StoreProcedure.sql
+++ Source/Query_StoreProcedure.sql
@@ -280,87 +280,18 @@ GO
 
 /** THIẾU: GET/Add REPEAT TIME*/
 
-EXEC dbo.sp_getRemindListByMecRcDetailsID @MecRcDetailsID = 1  -- int
-
--- add remind
-EXEC dbo.sp_REMIND_RemindDetails_AddNewRemind @MecRcDtID = 2, -- int
-    @remindID = NULL, -- int
-    @TimeRemind = '6:00', -- time(7)
-    @isRepeat = 0, -- int
-    @sound = '', -- varchar(30)
-    @label = '', -- varchar(30)
-    @isActivate = NULL -- bit
-
--- GET METHOD
-EXEC dbo.sp_Get_Search_MedicalRecordDetails @MecRcID = NULL, -- int
-    @DoctorID = 4, -- int
-    @DayCreated = '24/07/2017' -- VARCHAR(15)
-
-EXEC dbo.sp_getPrecriptionByMecRcDetailsID @MecRcDtID = 2 -- int
-GO
 
 --DROP PROCEDURE dbo.sp_getPrecriptionByMecRcDetailsID
 
 --DROP PROCEDURE dbo.sp_PRESCRIPTION_AddNewIncription
 SELECT * FROM dbo.USER_DrCare
 SELECT * FROM dbo.DISEASE
-
+SELECT * FROM dbo.REMIND
 SELECT * FROM dbo.MEDICAL_RECORD_DETAILS
 SELECT * FROM dbo.PRESCRIPTION
 GO
 
--- addd precription
-EXEC dbo.sp_PRESCRIPTION_AddNewIncription @MecRcDtID = 1, -- int
-    @MedID = 6, -- int
-    @MedQty = 1.0, -- float
-    @TimeTakeMedicine = 3, -- smallint
-    @SumMedQty = 21 -- smallint
 
-
-EXEC dbo.sp_PRESCRIPTION_AddNewIncription @MecRcDtID = 1, -- int
-    @MedID = 3, -- int
-    @MedQty = 1.0, -- float
-    @TimeTakeMedicine = 2, -- smallint
-    @SumMedQty = 14 -- smallint
-
-
-EXEC dbo.sp_PRESCRIPTION_AddNewIncription @MecRcDtID = 1, -- int
-    @MedID = 12, -- int
-    @MedQty = 1.0, -- float
-    @TimeTakeMedicine = 2, -- smallint
-    @SumMedQty = 14 -- smallint
-
-
-EXEC dbo.sp_PRESCRIPTION_AddNewIncription @MecRcDtID = 2, -- int
-    @MedID = 9, -- int
-    @MedQty = 1.0, -- float
-    @TimeTakeMedicine = 2, -- smallint
-    @SumMedQty = 12 -- smallint
-
-
-EXEC dbo.sp_PRESCRIPTION_AddNewIncription @MecRcDtID = 2, -- int
-    @MedID = 14, -- int
-    @MedQty = 1.0, -- float
-    @TimeTakeMedicine = 1, -- smallint
-    @SumMedQty = 6 -- smallint
-GO
-
--- ADD MEDICAL_RECORD
-EXEC dbo.sp_MEDICAL_RECORD_DETAILS_AddNewMedicalRecord @MecRcID = 11, -- int
-    @DiseaseID = 6, -- int
-    @DoctorID = 1 -- int
-
-EXEC dbo.sp_MEDICAL_RECORD_DETAILS_AddNewMedicalRecord @MecRcID = 10, -- int
-    @DiseaseID = 2, -- int
-    @DoctorID = 4 -- int
-
-EXEC dbo.sp_MEDICAL_RECORD_DETAILS_AddNewMedicalRecord @MecRcID = 9, -- int
-    @DiseaseID = 4, -- int
-    @DoctorID = 4 -- int
-GO
-
-EXEC dbo.sp_getPrecriptionByMecRcDetailsID @MecRcDtID = 2 -- int
-GO
 
 
 -- TRANSACTION
